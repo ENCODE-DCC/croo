0af6a264d801e84320d7070503718100bb1db570
Jin Lee <leepc12@gmail.com>
2019-10-24 18:07:06 -0700

    pip: remove long description and just link it to github

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     author='Jin Lee',
     author_email='leepc12@gmail.com',
     description='CRomwell Output Organizer',
-    long_description=long_description,
+    long_description='https://github.com/ENCODE-DCC/croo',
     long_description_content_type='text/markdown',
     url='https://github.com/ENCODE-DCC/croo',
     packages=setuptools.find_packages(exclude=['examples', 'docs']),
