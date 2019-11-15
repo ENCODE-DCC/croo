bb9bbbf2cbac6eafef65b21ddd98a92e6738d11a
Jin Lee <leepc12@gmail.com>
2019-11-15 09:39:37 -0800

    ver: ->0.3.0

--- croo/croo_args.py
+++ croo/croo_args.py
@@ -13,7 +13,7 @@ from caper.caper_uri import (
     MAX_DURATION_SEC_PRESIGNED_URL_GCS)
 
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 def parse_croo_arguments():
     """Argument parser for Cromwell Output Organizer (COO)
